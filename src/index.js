2d843fbfd56cf8965e006c9e65e75bc0e2c1a652
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-02-03 11:07:15 +0100

    leccion 4.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const moviesJson = require("../web/src/data/movies.json");
+const express = require('express');
+const cors = require('cors');
+const moviesJson = require('../web/src/data/movies.json');
 // create and config server
 const server = express();
 server.use(cors());
@@ -12,11 +12,15 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", (req, res) => {
+server.get('/movies', (req, res) => {
   const response = {
     success: true,
-    movies: moviesJson
+    movies: moviesJson,
   };
 
   res.json(response);
 });
+
+//Servidor de estaticos
+const staticServerPath = './src/public-react';
+server.use(express.static(staticServerPath));
