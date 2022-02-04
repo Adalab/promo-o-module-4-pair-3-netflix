31925275f39271993c5f215655a8ad590e16bee9
mireiasuefra <mireia.suefra@gmail.com>
2022-02-04 10:59:26 +0100

    4.4 Express JS III

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const moviesJson = require('../web/src/data/movies.json');
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.set("view engine", "ejs");
+
+
+
 
 // init express aplication
 const serverPort = 4000;
@@ -24,3 +28,17 @@ server.get('/movies', (req, res) => {
 //Servidor de estaticos
 const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
+
+
+server.get('/movies/:moviesId', (req, res) => {
+
+  const requestParamsMovie = req.params.moviesId
+
+  const moviesData = moviesJson.find( movie => movie.id === requestParamsMovie);
+
+  console.log(moviesData);
+
+ res.render("movie", moviesData)
+
+
+});
