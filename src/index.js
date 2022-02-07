35bf5ccab36eb687dd52fe7f5d08efd7c1af93ce
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-02-07 11:36:13 +0100

    leccion 4.5

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const moviesJson = require('../web/src/data/movies.json');
+const Database = require('better-sqlite3');
+
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.set("view engine", "ejs");
-
-
-
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -17,11 +16,16 @@ server.listen(serverPort, () => {
 });
 
 server.get('/movies', (req, res) => {
+  // preparamos y ejecutamos la query
+  const query = db.prepare('SELECT * FROM movies ORDER BY title ?');
+
+  const movies = query.all(req.query.sort);
+  // respondemos a la petición con los datos que ha devuelto la base de datos
+
   const response = {
     success: true,
-    movies: moviesJson,
+    movies: movies,
   };
-
   res.json(response);
 });
 
@@ -29,16 +33,17 @@ server.get('/movies', (req, res) => {
 const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
 
-
 server.get('/movies/:moviesId', (req, res) => {
+  const requestParamsMovie = req.params.moviesId;
 
-  const requestParamsMovie = req.params.moviesId
-
-  const moviesData = moviesJson.find( movie => movie.id === requestParamsMovie);
+  const moviesData = moviesJson.find(
+    (movie) => movie.id === requestParamsMovie
+  );
 
   console.log(moviesData);
 
- res.render("movie", moviesData)
-
-
+  res.render('movie', moviesData);
 });
+
+//BASE DE DATOS
+const db = new Database('./src/db/database.db', { verbose: console.log });
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,10 +1,10 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//localhost:4000/movies', { method: "GET"}) // con esto obtenemos los datos (method)
-    .then(response => response.json())
+  return fetch(`//localhost:4000/movies?sort=${params.sort}`, { method: 'GET' }) // con esto obtenemos los datos (method)
+    .then((response) => response.json())
     .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return data;
@@ -12,7 +12,7 @@ const getMoviesFromApi = () => {
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
